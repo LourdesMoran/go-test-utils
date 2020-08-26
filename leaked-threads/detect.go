827f60b3a5151b301ba3f4446892c995dbee80c6
lourdesmm90 <lourdesmm90@gmail.com>
2020-08-26 09:48:48 -0400

    wip

--- leaked-threads/detect.go
+++ leaked-threads/detect.go
@@ -1,57 +1,71 @@
 package leaked_threads
 
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"runtime"
+	"runtime/pprof"
+	"strings"
+	"testing"
+	"time"
+)
+
 // TestingInspector tools to examine running tests suites
 type TestingInspector struct{}
 
-// RunWithGoroutineLeakageDetection has the goal of detecting potential Goroutines Leakage while running tests suites.
-// It runs the given test suite and compares the amount of Goroutines previous and after the tests.
-// If an increase of Goroutines is detected after the test suite finished running, it prints a warning about the
+// RunWithGoroutineLeakageDetection detects potential Goroutines Leakage while running tests suites.
+// Runs the given test and compares the amount of Goroutines previous and after the tests are ran.
+// If an increase of Goroutines is detected, prints a warning about the
 // specific test with the potential Goroutine Leakage and a stacktrace of the Goroutines.
 // Params:
+// testName: Name of the test
 // t: testing context
-// testSuite: test suite to run
-// SuiteName: Name of the test suite
-func (i *TestingInspector) RunWithGoroutineLeakageDetection(t *testing.T, testSuite suite.TestingSuite, suiteName string) {
-	startingGoroutineNumber := runtime.NumGoroutine()
-	var b bytes.Buffer
-	_ = pprof.Lookup("goroutine").WriteTo(&b, 1)
-	startStackTrace := i.goroutineStackTraceToMap(b.String())
-
+// testFunc: test function
+func (i *TestingInspector) RunWithGoroutineLeakageDetection(testName string, t *testing.T, testFunc func(t *testing.T)) {
+	initialStackTrace, initialGoroutineNumber := i.GetGoroutinesStackTrace()
 	// Run test suite
-	suite.Run(t, testSuite)
-	time.Sleep(1 * time.Second) // A brief moment for all pending goroutines to finish
+	t.Run(testName, testFunc)
+	time.Sleep(1 * time.Second) // A moment for all pending goroutines to finish
+	finalStackTrace, finalGoroutineNumber := i.GetGoroutinesStackTrace()
+	log.Printf("%s: %s %d %d",
+		"goroutine-leakage-detection", "Initial and Final number of goroutines after running test: ",
+		initialGoroutineNumber, finalGoroutineNumber)
 
 	// Compare Goroutines from before and after the test suite was ran
-	endingGoroutineNumber := runtime.NumGoroutine()
-	if endingGoroutineNumber-startingGoroutineNumber > 0 {
-		// Print only the new goroutines created during the test suite run that are still alive.
-		b.Truncate(0)
-		err := pprof.Lookup("goroutine").WriteTo(&b, 1)
-		if err != nil {
-			log.Logger.WithPrefix("goroutine-leakage-detection").Error(err)
+	// Print only the new goroutines created during the test suite run that are still alive.
+	var newGoroutinesStackTrace string
+	// filter results
+	for endKey := range finalStackTrace {
+		if i.isKeyInMap(endKey, initialStackTrace) {
+			continue
 		}
-		endStackTrace := i.goroutineStackTraceToMap(b.String())
-		var newGoroutinesStackTrace string
-		// filter results
-		for endKey := range endStackTrace {
-			if i.isKeyInMap(endKey, startStackTrace) {
-				continue
-			}
-			if i.ignoredGoroutines(endStackTrace[endKey]) {
-				// Continue on goroutines that ar known to not terminate safely
-				continue
-			}
-			newGoroutinesStackTrace += endStackTrace[endKey]
+		if i.ignoredGoroutines(finalStackTrace[endKey]) {
+			// Continue on goroutines that ar known to not terminate safely
+			continue
 		}
-		if newGoroutinesStackTrace != "" {
-			// Print to stdout
-			panic(fmt.Sprintf(
-				"Potential Goroutines Leakage detected in %s. See the following stack trace: \n %v",
-				suiteName,
-				newGoroutinesStackTrace,
-			))
-		}
+		newGoroutinesStackTrace += finalStackTrace[endKey]
 	}
+	if newGoroutinesStackTrace != "" {
+		// Print to stdout
+		panic(fmt.Sprintf(
+			"Potential Goroutines Leakage detected in %s. See the following stack trace: \n %v",
+			testName,
+			newGoroutinesStackTrace,
+		))
+	}
+}
+
+func (i *TestingInspector) GetGoroutinesStackTrace() (goroutines map[string]string, total int) {
+	startingGoroutineNumber := runtime.NumGoroutine()
+	var b bytes.Buffer
+	err := pprof.Lookup("goroutine").WriteTo(&b, 1)
+	if err != nil {
+		log.Printf("%s: %s", "goroutine-leakage-detection", err.Error())
+	}
+	startStackTrace := i.goroutineStackTraceToMap(b.String())
+
+	return startStackTrace, startingGoroutineNumber
 }
 
 func (i *TestingInspector) goroutineStackTraceToMap(stackTrace string) (goroutines map[string]string) {
@@ -83,8 +97,7 @@ func (i *TestingInspector) ignoredGoroutines(stack string) bool {
 		// these leveldb goroutines are async terminated even after calling db.Close,
 		// so they may still alive after test end, thus cause false positive leak failures.
 		// sleeping after closing without knowing how much is not a good solution
-		// FIXME: Investigate a better way to handle these.
-		// 	      There are several other projects on GitHub using goleveldb with this issue.
+		// FIXME: Investigate a better way to handle these. There are several other projects on GitHub using goleveldb with this issue.
 		"leveldb/util.(*BufferPool).drain",
 		"goleveldb/leveldb.(*DB).compactionError",
 		"goleveldb/leveldb.(*DB).mCompaction",
